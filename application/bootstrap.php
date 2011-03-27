534362618f4b9a7c088aa73b4cc31761f0e3a551
Jeremy Lindblom <jeremeamia@gmail.com>
2011-03-26 21:44:14 -0700

    Stubbed actions for the story controller. Made a story route.

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -111,8 +111,8 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
+Route::set('story', '(story(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'welcome',
+		'controller' => 'story',
 		'action'     => 'index',
 	));
---
+++ application/classes/controller/story.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Story extends Controller {
+
+	public function action_index()
+	{
+		$this->response->body('hello, world!');
+	}
+
+	public function action_view()
+	{
+		$this->response->body('hello, world!');
+	}
+
+	public function action_add()
+	{
+		$this->response->body('hello, world!');
+	}
+
+	public function action_edit()
+	{
+		$this->response->body('hello, world!');
+	}
+
+	public function action_delete()
+	{
+		$this->response->body('hello, world!');
+	}
+
+} // End Story
