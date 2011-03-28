8cbce3c5b3d3e45a224c6fa1342b2e7ddd30b235
Jeremy Lindblom <jeremeamia@gmail.com>
2011-03-28 10:11:15 -0700

    Removing a comment and adjusting some spacing

--- application/views/story/index.php
+++ application/views/story/index.php
@@ -1,5 +1,4 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
 <!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
 <!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
 <!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
@@ -13,18 +12,18 @@
 </head>
 <body id="<?php echo trim($request->directory().'_'.$request->controller().'_'.$request->action(), '_'); ?>" class="no-layout">
   <div id="main" role="main">
-	<h1><?php echo $title; ?></h1>
-	<?php if ($stories): ?>
-	  <div class="stories">
-	    <div class="story">
+    <h1><?php echo $title; ?></h1>
+    <?php if ($stories): ?>
+      <div class="stories">
+        <div class="story">
           STORY
-	    </div>
-	  </div>
-	<?php else: ?>
-	  <div class="no-stories">
-	    NO STORIES
-	  </div>
-	<?php endif; ?>
+        </div>
+      </div>
+    <?php else: ?>
+      <div class="no-stories">
+        NO STORIES
+      </div>
+    <?php endif; ?>
   </div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
   <script>window.jQuery || document.write("<script src='js/libs/jquery-1.5.1.min.js'>\x3C/script>")</script>
