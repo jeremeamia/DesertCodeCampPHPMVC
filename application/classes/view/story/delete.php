85f791f5bf7197294e3c90908982c7f17020eff5
Lorenzo Pisani <zeelot3k@gmail.com>
2011-03-30 19:31:48 -0700

    adding views for edit and delete

---
+++ application/classes/view/story/delete.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class View_Story_Delete extends View_Page {}
---
+++ application/classes/view/story/edit.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class View_Story_Edit extends View_Page {}
