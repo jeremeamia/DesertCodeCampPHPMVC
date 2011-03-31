a917156bf078c05385831ae9b3dd43c5e8fb037f
Lorenzo Pisani <zeelot3k@gmail.com>
2011-03-30 19:56:56 -0700

    adding a couple missing titles

--- application/messages/titles.php
+++ application/messages/titles.php
@@ -2,9 +2,10 @@
 
 return array(
 	'story' => array(
-		'index'      => 'User Stories',
-		'view'       => 'Story #:id',
-		'add'        => 'Add User Story',
-		'incomplete' => 'Incomplete Page',
+		'index'  => 'User Stories',
+		'view'   => 'Story #:id',
+		'add'    => 'Add User Story',
+		'edit'   => 'Incomplete Page',
+		'delete' => 'Incomplete Page',
 	),
 );
