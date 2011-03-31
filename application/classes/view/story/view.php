138998d71cfb04167da8da28eeccdac10587155d
Lorenzo Pisani <zeelot3k@gmail.com>
2011-03-30 19:52:56 -0700

    moving story/view to kostache

--- application/classes/view/story/view.php
+++ application/classes/view/story/view.php
@@ -1,6 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class View_Story_View extends View_Page
-{
+class View_Story_View extends View_Page {
+
+	public function title()
+	{
+		return __(Kohana::message('titles', 'story.view'), array(':id' => $this->story->id));
+	}
+
+	public function story()
+	{
+		$story = $this->story->as_array();
+		$story['notes'] = nl2br(HTML::chars($this->story->notes));
+
+		return $story;
+	}
 
 }
